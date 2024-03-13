bc4440947750a9186d75d8c07fb7be079e48afb1
William Smith <WilliamSmith107@icloud.com>
2024-03-13 18:14:13 +0000

    Initial Commit

---
+++ Shuffle Focus/Shuffle_FocusApp.swift
@@ -0,0 +1,17 @@
+//
+//  Shuffle_FocusApp.swift
+//  Shuffle Focus
+//
+//  Created by William Smith on 13/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct Shuffle_FocusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Shuffle Focus/Views/TimerView.swift
@@ -0,0 +1,18 @@
+//
+//  TimerView.swift
+//  Shuffle Focus
+//
+//  Created by William Smith on 13/03/2024.
+//
+
+import SwiftUI
+
+struct TimerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TimerView()
+}
